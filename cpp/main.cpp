16226f8cf56a835e6ed402bb4d5e62af63ccebe7
huttongrabiel <huttonthomas@icloud.com>
2021-09-26 15:34:11 -0700

    began work on ui in main.cpp. need to fix check on if int provided

---
+++ cpp/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+#include "include.h"
+#include <typeinfo>
+
+// Driver Code
+int main() {
+  
+  int coefficient, exponent;
+
+  std::cout << "Enter Your Equation On Next Line" << "\n";
+  std::cout << "--------------------------------" << "\n";
+
+  std::cout << "Coefficient: ";
+  std::cin >> coefficient;
+
+//  while (typeid(coefficient).name() != "i") {
+//    std::cout << "ERROR: value entered must be of type int" << "\n";
+//    std::cout << "Coefficient: ";
+//    std::cin >> coefficient;
+//  }
+
+  std::cout << "Exponent: ";
+  std::cin >> exponent;
+//  while (typeid(coefficient).name() != "i") {
+//    std::cout << "ERROR: value entered must be of type int" << "\n";
+//    std::cout << "Exponent: ";
+//    std::cin >> exponent;
+//  }
+
+  std::cout << "Equation: " << coefficient << "x^" << exponent << "\n";
+}
