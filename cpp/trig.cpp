aee1f19b0d51d0fd3e59c2b4ba7f645f261e4e4d
huttongrabiel <huttonthomas@icloud.com>
2021-09-30 17:46:32 -0700

    TrigonometricDerivatives() function created, not done yet, still in progress

---
+++ cpp/trig.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "include.h"
+
+void TrigonometricDerivatives() {
+  std::map<string, string> trigDerivs;
+
+  // Populating the map with values
+  // sin -> cos
+  // cos -> -sin
+  // tan -> sec^2
+  // cot -> -csc^2
+  // cscx -> -cscxcotx
+  // secx -> secxtanx
+  
+  trigDerivs.insert(pair<string,string>("sin(x)", "cos(x)");
+     
+} 
