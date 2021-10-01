caafaf7c475b8fb3c5f372b7dc8a074032dfce6e
huttongrabiel <huttonthomas@icloud.com>
2021-09-30 19:59:26 -0700

    implemented trigonometricDerivative(std::string) function for the six basic trig derivs

--- cpp/trig.cpp
+++ cpp/trig.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "include.h"
+#include <map>
 
-void TrigonometricDerivatives() {
-  std::map<string, string> trigDerivs;
+void trigonometricDerviative(std::string) {
+  std::map<std::string, std::string> trigDerivs;
 
   // Populating the map with values
   // sin -> cos
@@ -11,7 +12,17 @@ void TrigonometricDerivatives() {
   // cot -> -csc^2
   // cscx -> -cscxcotx
   // secx -> secxtanx
-  
-  trigDerivs.insert(pair<string,string>("sin(x)", "cos(x)");
-     
+  trigDerivs.insert(std::pair<std::string, std::string>("sin(x)", "cos(x)")); 
+  trigDerivs.insert(std::pair<std::string, std::string>("cos(x)", "-sin(x)")); 
+  trigDerivs.insert(std::pair<std::string, std::string>("tan(x)", "sec^2(x)")); 
+  trigDerivs.insert(std::pair<std::string, std::string>("cot(x)", "-csc^2(x)")); 
+  trigDerivs.insert(std::pair<std::string, std::string>("csc(x)", "-csc(x)cot(x)")); 
+  trigDerivs.insert(std::pair<std::string, std::string>("sec(x)", "sec(x)tan(x)")); 
+
+  std::map<std::string, std::string>::iterator itr;
+  for (itr = trigDerivs.begin(); itr != trigDerivs.end(); itr++) {
+    if (itr->first == user_trig_function) {
+      std::cout << itr->second << "\n";
+    }
+  } 
 } 
