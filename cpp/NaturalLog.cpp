ce81204561210e40988cdd1c3c390d4b39d3a46e
huttongrabiel <huttonthomas@icloud.com>
2021-10-05 21:34:40 -0700

    new natural log function, still need to add to header file, implement code, integrate with main

---
+++ cpp/NaturalLog.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() { // only int main for now because i want to test before switching to normal function declaration
+  // This function will calculate the deriviative of ln(u). d/dx ln(u) = 1/u du/dx
+  
+  int u;
+  std::cout << "Enter U function (ln(u)): ";
+  std::cin >> u;
+  std::cout << "Calculating: ln(" << u << ")" << "\n";
+
+
+}
