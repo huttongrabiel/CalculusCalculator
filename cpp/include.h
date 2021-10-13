bba97e0ad003b2f92961e0918c105bbb414438cb
huttongrabiel <huttonthomas@icloud.com>
2021-10-12 22:17:13 -0700

    added new functions to header file

--- cpp/include.h
+++ cpp/include.h
@@ -1,9 +1,10 @@
 #ifndef INCLUDE_H
 #define INCLUDE_H
 
-void powerRule(int);
+void powerRule();
 void displayAnswer(int, int);
 void trigonometricDerivative();
-void powerRule(int coefficient, int exponent); // overloaded function 
+void powerRuleWithTwoParameters(int, int); // overloaded function 
+void naturalLogOfU();
 
 #endif
